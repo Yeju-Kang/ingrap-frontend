b5d42d54b21692da646cd599d5c77abb67077273
Yeju-Kang <bbogaeme@gmail.com>
2025-03-06 11:21:09 +0900

    방에 가구 추가하고, 움직임 기능 추가

--- src/pages/ProjectPage/ProjectPage.js
+++ src/pages/ProjectPage/ProjectPage.js
@@ -5,7 +5,6 @@ import FilterPanel from "./FilterPanel";
 import ProductList from "./ProductList";
 import RoomArea from "./RoomArea";
 
-
 const ProjectPage = () => {
   const [filters, setFilters] = useState({
     brand: "ALEX MULLER",
@@ -15,13 +14,19 @@ const ProjectPage = () => {
     price: [0, 10000000],
   });
 
+  const [furnitureList, setFurnitureList] = useState([]); // ✅ 선택된 가구 리스트
+
+  const handleAddFurniture = (furniture) => {
+    setFurnitureList((prev) => [...prev, furniture]);
+  };
+
   return (
     <Box display="flex" height="100vh" sx={{ marginTop: "80px" }}>
       <Sidebar />
-      <RoomArea />
+      <RoomArea furnitureList={furnitureList} />
       <Box width="300px" display="flex" flexDirection="column">
         <FilterPanel filters={filters} setFilters={setFilters} />
-        <ProductList />
+        <ProductList onAddFurniture={handleAddFurniture} />
       </Box>
     </Box>
   );
