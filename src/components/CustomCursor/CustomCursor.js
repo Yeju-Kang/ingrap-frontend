e1db4e7f7a62ab84a62e22157ea217015ab6bd3b
Yeju-Kang <bbogaeme@gmail.com>
2025-04-18 01:26:08 +0900

    커서 오류 수정

--- src/components/CustomCursor/CustomCursor.js
+++ src/components/CustomCursor/CustomCursor.js
@@ -1,25 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./CustomCursor.scss";
 
 function CustomCursor() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [clickEffect, setClickEffect] = useState(false);
+  const animationFrameRef = useRef(null);
+  const latestPositionRef = useRef({ x: 0, y: 0 });
+  const timeoutRef = useRef(null);
 
   useEffect(() => {
     const moveCursor = (e) => {
-      const { clientX, clientY } = e;
-      // 좌표가 바뀌었을 때만 setState
-      setPosition((prev) => {
-        if (prev.x !== clientX || prev.y !== clientY) {
-          return { x: clientX, y: clientY };
-        }
-        return prev;
-      });
+      latestPositionRef.current = { x: e.clientX, y: e.clientY };
+
+      if (!animationFrameRef.current) {
+        animationFrameRef.current = requestAnimationFrame(() => {
+          setPosition(latestPositionRef.current);
+          animationFrameRef.current = null;
+        });
+      }
     };
 
     const handleClick = () => {
       setClickEffect(true);
-      setTimeout(() => setClickEffect(false), 300);
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = setTimeout(() => {
+        setClickEffect(false);
+      }, 300);
     };
 
     window.addEventListener("mousemove", moveCursor);
@@ -28,6 +34,8 @@ function CustomCursor() {
     return () => {
       window.removeEventListener("mousemove", moveCursor);
       window.removeEventListener("mousedown", handleClick);
+      cancelAnimationFrame(animationFrameRef.current);
+      clearTimeout(timeoutRef.current);
     };
   }, []);
 
